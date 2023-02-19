217b60f61bdd7dfcd4d39cb549b2cd443570f7fb
Bibbleboop <nicjah@hotmail.com>
2023-02-19 18:43:06 +0000

    Initial js files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Valid API Key
+let APIKEY = "d23c1342fed6bb5821f38a930fb0894b";
+let APIkey = "06191c32d8ecb0ee651d31bd50188fd1";
+
+
+// Database addresss to retrieve data from 
+let queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "city" + "APIKEY";
+
+//
+
+function SearchOpenWeatherMap(city) {
+
+}
+
