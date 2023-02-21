c55dede32dd6803952999faad31b122aedb9434f
Bibbleboop <nicjah@hotmail.com>
2023-02-21 00:06:17 +0000

    function test & console logs

--- js/script.js
+++ js/script.js
@@ -1,43 +1,64 @@
 // Valid API Key for relevant URL
 // let APIKEY = "d23c1342fed6bb5821f38a930fb0894b";
-let APIKEY = "06191c32d8ecb0ee651d31bd50188fd1";
+let APIkey = "06191c32d8ecb0ee651d31bd50188fd1";
 
 // Database addresss to retrieve data from includes URL address, search criteria & API Key
-let queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "city" + APIKEY;
+// let queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "city" + APIKEY;
+// let queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={APIkey}";
+let today = moment();
+$("check").text(today.format("D MMM YYYY"));
+
+let queryURL = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&appid={APIkey}";
+let url = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q={city name"
 
 // Run the AJAX call to the OpenWeatherMap API
 $.ajax({
-    url:queryURL,
-    method: "GET"
-})
+    type: 'GET';
+    url: queryURL;
+    
+    // Type: 'json',
+    // success: function(data) {}
+// //console.log(forecastjson)
+// displayWeather(forecastjson);
+// }, 
+// error: function(error) {
+//     console.log(error);
 
-// Store retrieved data inside of ab object called response
-.then(function(response) {
+// }
+// });
+    // Store retrieved data inside of ab object called response
+}).then(function(response) {
 
 // Log the queryURL 
 console.log(queryURL);
 
-//The associated Object
+//The Resulting Object
 console.log(response);
+
+
+
+// Show results on webpage HTML
+$("#city").text("<h1>" + response.name + "Weather Details</h1>");
+$("#date").html("<h1>" + response.date + "Date</h1>");
+$("#icon").text(+ response.icon + "Icon");
+$("#temp").text("Temperature (K)" + response.main.temp);
+$("#temp").text("Temperature (C)" + response.toFixed(2));
 
 //convert to celcius
 let tempC = (response.main.temp);
 
-// Show results on webpage HTML
-$("city").html("<h1>" + response.name + "Weather Details</h1>");
-$("date").html("<h1>" + response.date + "Date</h1>");
-$("icon").img(+ response.icon + "Icon");
-$("temp").text("Temperature (K)" + response.main.temp);
-$("temp").text("Temperature (C)" + response.toFixed(2));
-$("wind").text("Wind Speed: " + response.wind.speed);
-$("humidity").text("Humidity: " + response.main.humidity);
-
+$("#wind").text("Wind Speed: " + response.wind.speed +  mph);
+$("#humidity").text("Humidity: " + response.main.humidity );
+""
 // Log data in console 
 console.log("Date: " + response.date);
 console.log("Icon  " + response.icon);
-console.log("Temperature (C): " tempC);
+// console.log("Temperature (C): " "tempC");
 console.log("Wind Speed: " + response.wind.speed);
 console.log("Humidity: " + response.main.humidity);
 
+console.log(response.main.temp - 272);
+
 });
 
+let Icon = data.weather[0].icon;
