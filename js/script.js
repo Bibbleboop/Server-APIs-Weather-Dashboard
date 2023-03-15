d8bf098a3bf1c579c3b0abc5e8ad311dbf7a6206
Bibbleboop <nicjah@hotmail.com>
2023-03-15 00:07:32 +0000

    main logic

--- js/script.js
+++ js/script.js
@@ -1,33 +1,63 @@
-// Valid API Key for relevant URL
-// let APIKEY = "d23c1342fed6bb5821f38a930fb0894b";
+// l searchinput = document.querySelector("#form-heading")
+let searchform = document.querySelector("#search-form")
+let searchanswer = document.querySelector(".form-input")
+// let searchanswer = document.querySelector("search-input")
+let searchbtn = document.querySelector("search-button")
+// l yourselection = document.querySelector("#history")
+// l filmdata = document.querySelector("#today")
+let tommorow=  document.querySelector("#forecast") 
+let citydata = document.querySelector("#cityweather") 
+// v year = document.querySelector('#citydata')
+let citytemp  = document.querySelector("#MainTemp")
+let = citywind.querySelector("#MainWind")
+let = cityhumidity.querySelector(".input-group.apend")
+let = container.querySelector(".parentContainer")
+// let.querySelector(".listgroup")
+
+// Valid API Key for relevant URL // let APIKEY = "d23c1342fed6bb5821f38a930fb0894b";
 let APIkey = "06191c32d8ecb0ee651d31bd50188fd1";
 
-// Database addresss to retrieve data from includes URL address, search criteria & API Key
-// let queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "city" + APIKEY;
-// let queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={APIkey}";
 let today = moment();
 $("check").text(today.format("D MMM YYYY"));
 
 let queryURL = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&appid={APIkey}";
 let url = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q={city name"
+let cities = []
+//store data for cities searched 
+let loadcities = function() {
+    let citiesRetrieved = localStorage.getItem("cities")
+    if(!citiesRetrieved) {
+        return false;
+    }
+    citiesRetrieved =JSON.parse(citiesRetrieved);
+    for (let i=0; i <citiesRetrieved.length; i++) {
+        createButton(citiesRetrieved[i])
+        cities.push(citiesRetrieved[i])
+    }
+}
+// save cities in local storage json stringify
+let savecities = function () {
+    localStorage.setItem("cities", JSON.stringify(cities));
 
-// Run the AJAX call to the OpenWeatherMap API
-$.ajax({
-    type: 'GET';
-    url: queryURL;
-    
-    // Type: 'json',
-    // success: function(data) {}
-// //console.log(forecastjson)
-// displayWeather(forecastjson);
-// }, 
-// error: function(error) {
-//     console.log(error);
+    event.preventDefault = function(event) {}
+}
 
-// }
-// });
+let searchfunction =function(event) {
+event.preventDefault();
+let city = searchanswer.ariaValueMax.trim();
+
+}
+// Fetch with url data
+fetch(queryURL)
     // Store retrieved data inside of ab object called response
-}).then(function(response) {
+.then(function(response) {
+//if response is ok
+if (response.ok) {
+    response.json().then(function(data) {
+        dashboard(data, city); 
+    });
+let priorSearch = 
+
 
 // Log the queryURL 
 console.log(queryURL);
@@ -59,6 +89,12 @@ console.log("Humidity: " + response.main.humidity);
 
 console.log(response.main.temp - 272);
 
-});
+};
 
 let Icon = data.weather[0].icon;
+
+//city history previously searched
+loadCities()
+
+searchbtn.addEventListener("click", searchfunction);
+
